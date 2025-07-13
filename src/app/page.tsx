da6e055561319d87abf28921190b4e56755c3aad
Hardcodepunk <howl@hardcodepunk.be>
2025-07-13 15:02:28 +0200

    fluid background update animated bands

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
 export default function HomePage() {
-  return (
-    <div style={{ position: "relative", zIndex: 1, color: "white", padding: "2rem" }}>
-      <h1>Hello Auroras</h1>
-      <p>This is your home page</p>
-    </div>
-  )
+  return <div style={{ position: "relative", zIndex: 1, color: "white", padding: "2rem" }}></div>
 }
