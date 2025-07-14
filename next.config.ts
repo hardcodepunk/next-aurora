a7e770aee94cdd87cf37d5c935a8049e75fbdc7f
Hardcodepunk <howl@hardcodepunk.be>
2025-07-14 23:09:48 +0200

    fix ts errors

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,16 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
+import type { NextConfig } from "next"
+import type { Configuration } from "webpack"
+
+const nextConfig: NextConfig = {
+  webpack(config: Configuration) {
+    config.module?.rules?.push({
       test: /\.(glsl|vs|fs|vert|frag)$/,
       exclude: /node_modules/,
-      use: {
-        loader: "raw-loader",
-      },
+      use: ["raw-loader"],
     })
+
     return config
   },
 }
 
-module.exports = nextConfig
+export default nextConfig
