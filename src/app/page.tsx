d26e2ecc40dc835d216502d140441b867cfa3169
Hardcodepunk <howl@hardcodepunk.be>
2025-07-16 17:43:53 +0200

    add who section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import About from "@/components/About"
 import Splash from "@/components/Splash"
+import Who from "@/components/Who"
 
 export default function HomePage() {
   return (
     <>
       <Splash />
       <About />
+      <Who />
     </>
   )
 }
---
+++ src/components/Who.tsx
@@ -0,0 +1,16 @@
+export default function About() {
+  return (
+    <div className="relative z-10 h-screen flex px-6 md:px-16 text-[#333]">
+      <div className="max-w-2xl text-left font-sirenia">
+        <h2 className="text-3xl md:text-4xl font-regular">Wie ben ik?</h2>
+        <p className="text-base md:text-lg font-thin leading-relaxed mt-4">
+          Ik ben Eva De Coster, klinisch psychologe en psychoanalytisch psychotherapeute.
+        </p>
+        <p className="text-base md:text-lg font-thin leading-relaxed mt-2">
+          Naast mijn werk in de praktijk ben ik afdelingspsychologe binnen het Psychiatrisch Ziekenhuis Onzelievevrouw
+          in Brugge.
+        </p>
+      </div>
+    </div>
+  )
+}
