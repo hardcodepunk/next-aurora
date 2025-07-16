b1977482f7d424c4e8ef40ce8446a56cf29a69bd
Hardcodepunk <howl@hardcodepunk.be>
2025-07-16 17:39:43 +0200

    styling about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,16 +2,16 @@ export default function About() {
   return (
     <div className="relative z-10 h-screen flex items-center px-6 md:px-16 text-[#333]">
       <div className="max-w-2xl text-left font-sirenia">
-        <h2 className="text-3xl md:text-4xl font-semibold">Wat kan therapie voor jou betekenen?</h2>
-        <p className="text-base md:text-lg leading-relaxed mt-4">
+        <h2 className="text-3xl md:text-4xl font-regular">Wat kan therapie voor jou betekenen?</h2>
+        <p className="text-base md:text-lg font-thin leading-relaxed mt-4">
           Iedereen botst vroeg of laat op psychische moeilijkheden. Als deze thema’s blijven terugkeren en je steeds
           meer belemmeren, ontstaat er lijden dat je niet zomaar kan negeren.
         </p>
-        <p className="text-base md:text-lg leading-relaxed mt-2">
+        <p className="text-base md:text-lg font-thin leading-relaxed mt-2">
           In gesprek gaan met iemand die zonder oordeel luistert, biedt dan verlichting. In een veilige ruimte vrijuit
           spreken helpt je om jezelf beter te begrijpen, en precies daarin ligt de kracht tot verandering.
         </p>
-        <blockquote className="bg-[#fff6f0] text-sm md:text-base italic rounded-md shadow-md px-4 py-3 mt-6">
+        <blockquote className="bg-[#fff6f0] font-thin md:text-base  rounded-md px-4 py-3 mt-6">
           Therapie wordt steeds persoonlijk afgestemd, met het verlangen die te samen vorm te geven, zodat er voldoende
           ruimte is om jouw unieke levensverhaal en vragen te verkennen.
         </blockquote>
